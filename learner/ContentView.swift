a78eb8a302c4cecb826ffe4f7a7df0c9ea802db4
nooreealotibi <nooreealotibi@gmail.com>
2022-12-11 10:51:08 +0300

    Initial Commit

---
+++ learner/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  learner
+//
+//  Created by noor alotibi on 17/05/1444 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ learner/learnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  learnerApp.swift
+//  learner
+//
+//  Created by noor alotibi on 17/05/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct learnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
